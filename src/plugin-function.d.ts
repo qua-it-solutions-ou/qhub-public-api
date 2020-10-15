e438b9ccda02428d12ed8b36ae0ccdfebe820e6b
Muhammet Ali AKBAY <me@muhammetaliakbay.com>
2020-10-15 18:28:21 +0300

    Added UIPluginFunction definitions

--- src/plugin-function.d.ts
+++ src/plugin-function.d.ts
@@ -3,15 +3,20 @@ import {PluginManagerHighway} from './plugins';
 import {ConnectionIdentifier, ConnectionManagerHighway} from './connections';
 import {DomainsHighway} from './domains';
 
+export type UIPluginHighway = HubPluginHighway & {
+    namespace(line: 'ui-plugin-manager'): PluginManagerHighway;
+};
 export type HubPluginHighway = Highway & {
     namespace(line: 'plugin-manager'): PluginManagerHighway;
     namespace(line: 'connection-manager'): ConnectionManagerHighway;
     namespace(line: 'connection'): ConnectionNamespaceHighway;
     namespace(line: 'domains'): DomainsHighway;
 };
-export type NodePluginHighway = Highway & {
+
+export type NodeHighway = Highway & {
     namespace(line: 'plugin-manager'): PluginManagerHighway;
 };
+export type NodePluginHighway = NodeHighway;
 
 export interface NodePluginHandles {
     highway: NodePluginHighway
@@ -21,17 +26,21 @@ export interface HubPluginHandles {
     highway: HubPluginHighway
 }
 
+export interface UIPluginHandles extends HubPluginHandles {
+    highway: UIPluginHighway
+}
+
 export interface NodePluginFunction {
     (pluginHandles: NodePluginHandles): Promise<void>;
 }
 export interface HubPluginFunction {
     (pluginHandles: HubPluginHandles): Promise<void>;
 }
+export interface UIPluginFunction {
+    (pluginHandles: UIPluginHandles): Promise<void>;
+}
 
-export type ConnectionHighway = Highway & {
-    namespace(line: 'plugin-manager'): PluginManagerHighway;
-};
-
+export type ConnectionHighway = NodeHighway;
 export type ConnectionNamespaceHighway = Highway & {
     namespace(connectionID: ConnectionIdentifier): ConnectionHighway;
 };
