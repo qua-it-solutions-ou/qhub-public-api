71aeec242e1e85934bf4b28775da0099f69bac7b
Muhammet Ali AKBAY <ben@muhammetaliakbay.com>
2020-11-27 21:33:46 +0300

    Changed window-manager / observeWindowState function result

--- src/window.d.ts
+++ src/window.d.ts
@@ -1,9 +1,11 @@
+import {Observable} from "rxjs";
+
 export type WindowIdentifier = string;
 export type WindowHash = string;
 
 export interface WindowManager {
     controlWindow(windowID: WindowIdentifier, command: 'minimize' | 'maximize' | 'unmaximize' | 'restore' | 'close'): Promise<void>;
-    getWindowState(windowID: WindowIdentifier): Promise<'minimized' | 'maximized' | 'windowed'>;
+    observeWindowState(windowID: WindowIdentifier): Observable<'minimized' | 'maximized' | 'windowed'>;
     createWindow(parentWindowID: WindowIdentifier, hash: WindowHash): Promise<WindowIdentifier>;
 }
 
