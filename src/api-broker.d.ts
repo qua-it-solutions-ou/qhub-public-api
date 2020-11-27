776d93240493a284a60f13449459f6cb00e871d0
Muhammet Ali AKBAY <ben@muhammetaliakbay.com>
2020-11-27 12:38:15 +0300

    Fix on defination file name

--- src/api-broker.d.ts
+++ src/api-broker.d.ts
@@ -2,8 +2,11 @@ import {Observable} from "rxjs";
 
 export type APIRequestHandler = (action: string, input: Observable<any>) => Observable<any>;
 
-export interface APIBroker {
+export interface RequestOnlyAPIBroker {
     request(action: string, input: Observable<any>): Observable<any>;
+}
+
+export interface APIBroker extends RequestOnlyAPIBroker {
     register(action: string, handler: APIRequestHandler): void;
     unregister(action: string, handler: APIRequestHandler): void;
 }
